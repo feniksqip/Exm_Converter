67bcb93e11c07fb6b4e197d29fa30ae0a3ff4b82
Admin <admin@Mac-Admin.local>
2015-01-16 19:58:40 +0300

    Added.

---
+++ Exm_Converter/Exm_Converter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Exm_Converter
+//
+//  Created by Admin on 16.01.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
